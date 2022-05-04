9eae57dda591dea4408850a6099e56281c7b51c1
YuBoGuo <1137957329@qq.com>
2022-05-04 15:10:07 +0800

    fix: admin

--- apps/checkout/admin.py
+++ apps/checkout/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from apps.checkout.models import DeliveryOptions
 
-admin.site.register(DeliveryOptions)
+
+@admin.register(DeliveryOptions)
+class DeliveryOptionsAdmin(admin.ModelAdmin):
+    list_display = ('delivery_name', 'delivery_price', 'delivery_method')
--- apps/sneaker/admin.py
+++ apps/sneaker/admin.py
@@ -5,15 +5,17 @@ from apps.sneaker.models import Brand, SneakerImage, SneakerSize, Sneaker
 
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description')
+    list_display = ('name',)
 
 
 class SneakerImageInline(admin.TabularInline):
     model = SneakerImage
+    extra = 1
 
 
 class SneakerSizeInline(admin.TabularInline):
     model = SneakerSize
+    extra = 1
 
 
 @admin.register(Sneaker)
@@ -22,3 +24,6 @@ class SneakerAdmin(admin.ModelAdmin):
         SneakerImageInline,
         SneakerSizeInline
     ]
+    list_display = ('title', 'brand', 'discount_price', 'market_price', 'is_active', 'click_num')
+    list_filter = ('brand', 'is_active')
+    list_per_page = 20
